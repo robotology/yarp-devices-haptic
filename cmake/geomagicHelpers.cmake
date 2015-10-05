19d70133691d29d4cdbdf7ec5188e7cd9a1f9b6f
Ugo Pattacini <ugo.pattacini@iit.it>
2015-10-05 21:09:09 +0200

    fixes

--- cmake/geomagicHelpers.cmake
+++ cmake/geomagicHelpers.cmake
@@ -2,11 +2,11 @@
 # Authors: Ugo Pattacini <ugo.pattacini@iit.it>
 # CopyPolicy: Released under the terms of the GNU GPL v2.0.
 
-file(WRITE  ${PROJECT_SOURCE_DIR}/cmake/${PROJECT_NAME}Config.cmake "# Copyright: (C) 2015 iCub Facility\n")
-file(APPEND ${PROJECT_SOURCE_DIR}/cmake/${PROJECT_NAME}Config.cmake "# Authors: Ugo Pattacini <ugo.pattacini@iit.it>\n")
-file(APPEND ${PROJECT_SOURCE_DIR}/cmake/${PROJECT_NAME}Config.cmake "# CopyPolicy: Released under the terms of the GNU GPL v2.0.\n")
-file(APPEND ${PROJECT_SOURCE_DIR}/cmake/${PROJECT_NAME}Config.cmake "\n")
-file(APPEND ${PROJECT_SOURCE_DIR}/cmake/${PROJECT_NAME}Config.cmake "if(NOT geomagic_FOUND)\n")
-file(APPEND ${PROJECT_SOURCE_DIR}/cmake/${PROJECT_NAME}Config.cmake "set(${PROJECT_NAME}_INCLUDE_DIRS ${CMAKE_INSTALL_PREFIX}/include/${PROJECT_NAME})\n")
-file(APPEND ${PROJECT_SOURCE_DIR}/cmake/${PROJECT_NAME}Config.cmake "    set(geomagic_FOUND TRUE)\n")
-file(APPEND ${PROJECT_SOURCE_DIR}/cmake/${PROJECT_NAME}Config.cmake "endif()\n")
+file(WRITE  ${PROJECT_SOURCE_DIR}/cmake/generated/${PROJECT_NAME}Config.cmake "# Copyright: (C) 2015 iCub Facility\n")
+file(APPEND ${PROJECT_SOURCE_DIR}/cmake/generated/${PROJECT_NAME}Config.cmake "# Authors: Ugo Pattacini <ugo.pattacini@iit.it>\n")
+file(APPEND ${PROJECT_SOURCE_DIR}/cmake/generated/${PROJECT_NAME}Config.cmake "# CopyPolicy: Released under the terms of the GNU GPL v2.0.\n")
+file(APPEND ${PROJECT_SOURCE_DIR}/cmake/generated/${PROJECT_NAME}Config.cmake "\n")
+file(APPEND ${PROJECT_SOURCE_DIR}/cmake/generated/${PROJECT_NAME}Config.cmake "if(NOT geomagic_FOUND)\n")
+file(APPEND ${PROJECT_SOURCE_DIR}/cmake/generated/${PROJECT_NAME}Config.cmake "set(${PROJECT_NAME}_INCLUDE_DIRS ${CMAKE_INSTALL_PREFIX}/include/${PROJECT_NAME})\n")
+file(APPEND ${PROJECT_SOURCE_DIR}/cmake/generated/${PROJECT_NAME}Config.cmake "    set(geomagic_FOUND TRUE)\n")
+file(APPEND ${PROJECT_SOURCE_DIR}/cmake/generated/${PROJECT_NAME}Config.cmake "endif()\n")
